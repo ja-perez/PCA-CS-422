f2f49fc95e9b14e8c62253bbe1490cd1febbd924
javierp621194 <javierp621194@gmail.com>
2022-12-17 14:45:22 -0800

    Added project documentation and PCA.py function skeletons.

---
+++ PCA.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as mpl
+
+
+def compute_covariance_matrix(Z):
+    pass
+
+
+def find_pcs(cov):
+    pass
+
+
+def project_data(Z, PCS, L):
+    pass
+
+
+def show_plot(Z, Z_star):
+    pass
